ad711d2c6091912dc6e263181e8e187df55676e1
travis1999 <trevoroguna@gmail.com>
2021-02-11 23:53:07 +0300

    fixed an error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,12 @@ int main(void)
 	for (i = 1; i <= 100; ++i)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		if ((i % 3 != 0) && (i % 5 != 0))
-			printf("%d ", i);
+			printf("%d", i);
+		printf(" ");
 	}
 	printf("\n");
 
