051546c7b5314e0d5f8f8bf23dc8ab68f0077ada
travis1999 <trevoroguna@gmail.com>
2021-02-17 10:25:33 +0300

    fixed code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,26 +7,38 @@
 
 void rev_string(char *s)
 {
+	int length, c;
+	char *begin, *end, temp;
 
-	char *tail;
-	char h;
-	char t;
+	length = _strlen(s);
+	begin  = s;
+	end    = s;
 
+	for (c = 0; c < length - 1; c++)
+		end++;
 
-	if (!s)
-		return;
-
-	*tail = s;
-
-	while (*tail)
-		++tail;
-	--tail;
-
-	for ( ; s < tail; ++s, --tail)
+	for (c = 0; c < length / 2; c++)
 	{
-		h = *s
-		t = *tail;
-		*s = t;
-		*tail = h;
+		temp   = *end;
+		*end   = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
 	}
+}
+
+/**
+ * _strlen - calculates lenght of string
+ * @pointer: string
+ * Return: length of string
+ */
+
+int _strlen(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+		c++;
+	return (c);
 }
