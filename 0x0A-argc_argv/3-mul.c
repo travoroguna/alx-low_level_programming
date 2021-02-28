548c998177fcf6a8725ecfd71a82c29cba51f65e
travis1999 <trevoroguna@gmail.com>
2021-02-28 15:37:35 +0300

    fixed error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,15 @@
 
 int main(int argc, char *argv[])
 {
+	int result;
+
 	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (-1);
 	}
 
-	int result = atoi(argv[1]) * atoi(argv[2]);
-
+	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
 }
