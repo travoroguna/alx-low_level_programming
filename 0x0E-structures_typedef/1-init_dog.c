3e7a3ab9c402847e3ecfed132e0e7ba6926d2630
travis1999 <trevoroguna@gmail.com>
2021-03-08 23:53:50 +0300

    repaired file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 #include "dog.h"
 
-
 /**
  * init_dog - function that initialize a variable of type struct dog
  * @d: pointer to the dog
@@ -9,9 +8,10 @@
  * @age: age of dog
  * @owner: owner of dog
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
