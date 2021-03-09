6126212ba4405d96fd8513ec2b002b8b9875a9d5
travis1999 <trevoroguna@gmail.com>
2021-03-09 12:26:18 +0300

    fixed code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -79,7 +79,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog->age = age;
 	new_dog->name = dog_name;
-	new_dog->owner = owner;
+	new_dog->owner = dog_owner;
 
 	return (new_dog);
 }
