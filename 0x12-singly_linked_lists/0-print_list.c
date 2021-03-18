66a75b14d96de77b1b8da82173f960cdee0effd3
travis1999 <trevoroguna@gmail.com>
2021-03-18 22:25:23 +0300

    corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,17 @@
 #include "lists.h"
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
+char *strdup(const char *s)
+{
+    size_t size = strlen(s) + 1;
+    char *p = malloc(size);
+    if (p)
+        memcpy(p, s, size);
+
+    return p;
+}
 
 /**
  * print_int - prints an integer
