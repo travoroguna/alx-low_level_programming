4e064e364554c76d082179dafe422d1ae212d790
travis1999 <trevoroguna@gmail.com>
2021-03-18 22:48:49 +0300

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,62 +1,5 @@
 #include "lists.h"
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-
-
-/**
- * strdup- prints an integer
- * @s: variable to be printed
- * Return: length of printed characters
- */
-
-char *strdup(const char *s)
-{
-	size_t size = strlen(s) + 1;
-	char *p = malloc(size);
-
-	if (p)
-		memcpy(p, s, size);
-	return (p);
-}
-
-/**
- * print_int - prints an integer
- * @var: variable to be printed
- * Return: length of printed characters
- */
-
-int print_int(long int var)
-{
-	long int count = 0;
-
-	if (var < 0)
-	{
-		_putchar('-');
-		count += 1;
-		var = -var;
-	}
-
-	if (var / 10)
-		count += print_int(var / 10);
-
-	_putchar(var % 10 + '0');
-	count += 1;
-
-	return (count);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include <stdio.h>
 
 
 /**
@@ -68,25 +11,15 @@ int _putchar(char c)
 
 void print_str(char *str, size_t len)
 {
-	size_t count;
 	int is_null = 0;
 
 	if (str == NULL)
 	{
-		str = "nil";
+		str = "(nil)";
 		is_null = 1;
 	}
 
-	_putchar('[');
-	print_int(is_null ? 0 : len);
-	_putchar(']');
-	_putchar(' ');
-
-
-	for (count = 0; count < len; count++)
-		_putchar(*(str + count));
-
-	_putchar('\n');
+	printf("[%ld] %s\n", is_null ? 0 : len, str);
 }
 
 
