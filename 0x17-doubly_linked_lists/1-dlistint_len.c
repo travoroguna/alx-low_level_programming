1948489f37ed553dc15e1e8a6fd10fe331b8c7a0
travis1999 <trevoroguna@gmail.com>
2021-05-02 18:06:16 +0300

    fixed code

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,26 +1,16 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * add_dnodeint - add a new node at the beginning of the list
- * @head: double pointer to the beginning of the list
- * @n: value to add to the new node
- * Return: pointer to the new node, or NULL on failure
+ * dlistint_len - count the number of elements
+ * @h: pointer to the beginning of the list
+ * Return: num of nodes
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	*head = new;
-	if (new->next != NULL)
-		(new->next)->prev = new;
-	return (new);
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
 }
