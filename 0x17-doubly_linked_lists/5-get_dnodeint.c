774441566aaabe82485d25dba582a2ee399cdf9a
travis1999 <trevoroguna@gmail.com>
2021-05-02 17:39:40 +0300

    initial commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h>"
+
+/**
+ * get_dnodeint_at_index - finds an element of a linked list
+ * @head: pointer to the beginning of the list
+ * @index: index of the node to get
+ * Return: pointer to required element | NULL on failure
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (index == 0)
+		return (head);
+
+	for (i = 0; i < index; i++)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
+}
